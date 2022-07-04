0db4841c0d95df5fd676ef328e161775336d6d15
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-04 17:26:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http') //1.Import http module 
+const hostname = 'localhost'
+
+const { loadUser } = require("./user_model")
+
+const port = process.env.PORT || 3000 //2.Setting up port
+
+const server = http.createServer((req, res) => { //3.Creating http server
+    const jsonContent = JSON.stringify(loadUser())
+    res.end(jsonContent)
+
+})
+
+server.listen(port, hostname, () => {   //4.Listening to http sever
+    console.log(`Server running at http://${hostname}:${port}`)
+})
